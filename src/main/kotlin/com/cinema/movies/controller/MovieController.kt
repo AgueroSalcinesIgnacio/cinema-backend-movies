18cd05934b422bfbc891153a22a4bf60ac090d8e
Ignacio Agüero Salcines <aguero.salcines.ignacio@outlook.es>
2024-09-18 14:15:25 +0200

    ADD: exceptions

--- src/main/kotlin/com/cinema/movies/controller/MovieController.kt
+++ src/main/kotlin/com/cinema/movies/controller/MovieController.kt
@@ -2,6 +2,8 @@ package com.cinema.movies.controller
 
 import com.cinema.movies.model.dto.MovieResponse
 import com.cinema.movies.service.MovieService
+import com.cinema.users.config.error.exception.GenericException
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,7 +12,13 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/movies")
 class MovieController(private val movieService: MovieService) {
     @GetMapping
-    fun getMovies(): List<MovieResponse> {
-        return movieService.getMovies()
+    fun getMovies(): ResponseEntity<List<MovieResponse>> {
+
+        return try {
+            ResponseEntity.ok(movieService.getMovies())
+        } catch (exception: Exception) {
+            throw GenericException(exception.message)
+        }
+
     }
 }
